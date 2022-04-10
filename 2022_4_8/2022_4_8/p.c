a69878ae643673dbf748fbd5f22b0345ab0a0dd7
xuanan-zhu <1063785553@qq.com>
2022-04-10 21:54:24 +0800

    4/10

---
+++ 2022_4_8/2022_4_8/p.c
@@ -0,0 +1,12 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//# include <stdio.h>
+//# include <stdlib.h>
+//# include <string.h>
+//int main()
+//{
+//	int a = 10;
+//	printf("%p\n", &a);
+//	int* pa = &a;
+//	printf("%d", (int)sizeof(int*));
+//	return 0;
+//}
